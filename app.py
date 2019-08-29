0c30a8b6c7f9d8533894a15a9598dc99b50212f9
James Swank <jmswank7@gmail.com>
2019-08-28 20:24:24 -0600

    adds table

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import psycopg2
-conn = psycopg2.connect(host='localhost', database='dvdrental', user='postgres', password='1234')
+conn = psycopg2.connect(host='localhost', database='tutorial', user='postgres', password='1234')
 cur = conn.cursor()
-cur.execute('SELECT * FROM notes')
-one = cur.fetchone()
-all = cur.fetchall()
+cur.execute("""CREATE TABLE users(
+id integer PRIMARY KEY,
+email text,
+name text,
+address text)""")
+conn.commit()
+

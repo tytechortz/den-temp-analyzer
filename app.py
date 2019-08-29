1a94be0cd81783bb8e856b5524811b62774000d2
James Swank <jmswank7@gmail.com>
2019-08-28 20:40:16 -0600

    first table

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import psycopg2
-conn = psycopg2.connect(host='localhost', database='tutorial', user='postgres', password='1234')
-cur = conn.cursor()
-cur.execute("""CREATE TABLE users(
-id integer PRIMARY KEY,
-email text,
-name text,
-address text)""")
-conn.commit()
+# conn = psycopg2.connect(host='localhost', database='tutorial', user='postgres', password='1234')
+# cur = conn.cursor()
+# cur.execute("""CREATE TABLE users(
+# id integer PRIMARY KEY,
+# email text,
+# name text,
+# address text)""")
+# conn.commit()
 
+conn = psycopg2.connect(host='localhost', database='denver_temps', user='postgres', password='1234')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE dly_avg_norm(
+id integer PRIMARY KEY,
+STATION text,
+DATE DATE,
+DLY_AVG_NORMAL float)""")
+conn.commit()

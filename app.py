7e10127e376bee8cd25193c6aab29f860cc75a99
James Swank <jmswank7@gmail.com>
2019-08-28 19:44:48 -0600

    connects to db

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 import psycopg2
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
+conn = psycopg2.connect(host='localhost', database='dvdrental', user='postgres', password='1234')
+cur = conn.cursor()
+cur.execute('SELECT * FROM notes')
+one = cur.fetchone()
+all = cur.fetchall()

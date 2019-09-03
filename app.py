ffbbe2fb332e91b00edc155d1d5fac1de83696e8
James Swank <jmswank7@gmail.com>
2019-09-03 12:06:56 -0600

    downloads data

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+# from datetime import datetime
+import time
+
+today = time.strftime("%Y-%m-%d")
+
+df = pd.read_csv('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=2019-01-01&endDate=' + today + '&units=standard').round(1)
+print(df.head())

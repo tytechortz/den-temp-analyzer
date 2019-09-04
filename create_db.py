f71c6e883b401a99fb61eba6c7edc34b5d37c44c
James Swank <jmswank7@gmail.com>
2019-09-04 13:26:22 -0600

    combines all norms into one table

--- create_db.py
+++ create_db.py
@@ -1,40 +1,34 @@
 import psycopg2
-# conn = psycopg2.connect(host='localhost', database='tutorial', user='postgres', password='1234')
-# cur = conn.cursor()
-# cur.execute("""CREATE TABLE users(
-# id integer PRIMARY KEY,
-# email text,
-# name text,
-# address text)""")
-# conn.commit()
+import pandas as pd
+import time
+import os
+
+today = time.strftime("%Y-%m-%d")
+
+df = pd.read_csv('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=2019-01-01&endDate=' + today + '&units=standard').round(1)
+
+
+
+
 
 conn = psycopg2.connect(host='localhost', database='denver_temps', user='postgres', password='1234')
 cur = conn.cursor()
-# cur.execute("""CREATE TABLE dly_avg_norm(
-# id integer PRIMARY KEY,
-# STATION text,
-# DATE DATE,
-# DLY_AVG_NORMAL float)""")
-# conn.commit()
 
 # cur.execute("""CREATE TABLE dly_max_norm(
-# id integer PRIMARY KEY,
+# id SERIAL PRIMARY KEY,
 # STATION text,
 # DATE DATE,
-# DLY_NORMAL_MAX float)""")
-# conn.commit()
+# DLY_NORMAL_MAX FLOAT(2),
+# DLY_NORMAL_MIN FLOAT(2),
+# DLY_NORMAL_AVG FLOAT(2))""")
 
-# cur.execute("""CREATE TABLE dly_min_norm(
-# id integer PRIMARY KEY,
-# STATION text,
-# DATE DATE,
-# DLY_NORMAL_MIN float)""")
-# conn.commit()
 
-cur.execute("""CREATE TABLE data(
-id integer PRIMARY KEY,
-STATION text,
-DATE DATE,
-TMAX int,
-TMIN int)""")
+with open('daily_normal_max.csv', 'r') as f:
+    next(f)
+    cur.copy_from(f, 'dly_max_norm', sep=',')
+# f = open(r'daily_normal_max.csv', 'r')
+# next(f)
+# cur.copy_from(f, 'dly_max_norm', sep=',')
+f.close()
+
 conn.commit()
---
+++ data.py
@@ -0,0 +1,7 @@
+class NormalTMax:
+    def __init__(self, station, date, tmax, id):
+        self.station = station
+        self.date = date
+        self.tmax = tmax
+        
+

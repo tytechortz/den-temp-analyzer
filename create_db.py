0bfb35e25c532a595fd5465554f77b8a5afaf303
James Swank <jmswank7@gmail.com>
2019-09-05 14:11:46 -0600

    starts sqlalchemy

--- create_db.py
+++ create_db.py
@@ -2,6 +2,8 @@ import psycopg2
 import pandas as pd
 import time
 import requests
+import sqlalchemy
+
 
 
 today = time.strftime("%Y-%m-%d")
@@ -9,14 +11,21 @@ today = time.strftime("%Y-%m-%d")
 df = pd.read_csv('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=1950-01-01&endDate=' + today + '&units=standard').round(1)
 
 
-print(df)
+import sqlalchemy
+
+engine = sqlalchemy.create_engine("postgresql://postgres:1234@localhost/denver_temps")
+con = engine.connect()
+
+print(engine.table_names())
+# print(df)
 
 # res = requests.get('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=1950-01-01&endDate=' + today + '&units=standard')
+# print(type(res.text))
 # print(res.text)
 
 
-# conn = psycopg2.connect(host='localhost', database='denver_temps', user='postgres', password='1234')
-# cur = conn.cursor()
+conn = psycopg2.connect(host='localhost', database='denver_temps', user='postgres', password='1234')
+cur = conn.cursor()
 
 # # cur.execute("""CREATE TABLE dly_max_norm(
 # # id SERIAL PRIMARY KEY,
@@ -27,12 +36,13 @@ print(df)
 # # DLY_NORMAL_AVG FLOAT(2))""")
 
 
-# with open('daily_normal_max.csv', 'r') as f:
+# with open('res', 'r') as f:
 #     next(f)
-#     cur.copy_from(f, 'dly_max_norm', sep=',')
-# f = open(r'daily_normal_max.csv', 'r')
+#     cur.copy_from(f, 'data', sep=',')
+# f = open(r'res', 'r')
 # next(f)
-# cur.copy_from(f, 'dly_max_norm', sep=',')
+# cur.copy_from(res, 'data', sep=',')
 # f.close()
 
-# conn.commit()
+
+conn.commit()

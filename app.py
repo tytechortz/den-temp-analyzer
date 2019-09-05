c9f5c048556009dafd42c6999dc88faeba922c14
James Swank <jmswank7@gmail.com>
2019-09-05 13:00:40 -0600

    reads data with requests

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
+from data import NormalTMax
+
+
 
 today = time.strftime("%Y-%m-%d")
 
--- create_db.py
+++ create_db.py
@@ -1,34 +1,38 @@
 import psycopg2
 import pandas as pd
 import time
-import os
+import requests
+
 
 today = time.strftime("%Y-%m-%d")
 
-df = pd.read_csv('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=2019-01-01&endDate=' + today + '&units=standard').round(1)
+df = pd.read_csv('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=1950-01-01&endDate=' + today + '&units=standard').round(1)
 
 
+print(df)
+
+# res = requests.get('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=1950-01-01&endDate=' + today + '&units=standard')
+# print(res.text)
 
 
+# conn = psycopg2.connect(host='localhost', database='denver_temps', user='postgres', password='1234')
+# cur = conn.cursor()
 
-conn = psycopg2.connect(host='localhost', database='denver_temps', user='postgres', password='1234')
-cur = conn.cursor()
-
-# cur.execute("""CREATE TABLE dly_max_norm(
-# id SERIAL PRIMARY KEY,
-# STATION text,
-# DATE DATE,
-# DLY_NORMAL_MAX FLOAT(2),
-# DLY_NORMAL_MIN FLOAT(2),
-# DLY_NORMAL_AVG FLOAT(2))""")
+# # cur.execute("""CREATE TABLE dly_max_norm(
+# # id SERIAL PRIMARY KEY,
+# # STATION text,
+# # DATE DATE,
+# # DLY_NORMAL_MAX FLOAT(2),
+# # DLY_NORMAL_MIN FLOAT(2),
+# # DLY_NORMAL_AVG FLOAT(2))""")
 
 
-with open('daily_normal_max.csv', 'r') as f:
-    next(f)
-    cur.copy_from(f, 'dly_max_norm', sep=',')
+# with open('daily_normal_max.csv', 'r') as f:
+#     next(f)
+#     cur.copy_from(f, 'dly_max_norm', sep=',')
 # f = open(r'daily_normal_max.csv', 'r')
 # next(f)
 # cur.copy_from(f, 'dly_max_norm', sep=',')
-f.close()
+# f.close()
 
-conn.commit()
+# conn.commit()

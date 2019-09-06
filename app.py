0e0c14a3c7537fc832679bb3f3036ed794ed8882
James Swank <jmswank7@gmail.com>
2019-09-06 14:55:01 -0600

    prints rows

--- app.py
+++ app.py
@@ -23,17 +23,25 @@ try:
                                   host = "localhost",
                                   database = "denver_temps")
     cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
+    postgreSQL_select_Query = "select * from temps"
+
+    cursor.execute(postgreSQL_select_Query)
+    print("Selecting rows from mobile table using cursor.fetchall")
+    temp_records = cursor.fetchall()
+
+    print("Print each row and it's columns values")
+    for row in temp_records:
+        print("Id = ", row[0], )
+        print("Station = ", row[1])
+        print("Date = ", row[2])
+        print("TMax = ", row[3])
+        
 except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
+    print ("Error while fetching data from PostgreSQL", error)
+ 
 finally:
     #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")

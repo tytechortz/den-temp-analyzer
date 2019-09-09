f1a5ca068b14108c0e00259f653b9be539651362
James Swank <jmswank7@gmail.com>
2019-09-09 13:09:06 -0600

    queries by year and adds to df

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-import pandas as pd
-import time
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
-from data import NormalTMax
+import pandas as pd
+import sqlite3
+from dash.dependencies import Input, Output, State
+import time
+# import datetime
+from datetime import datetime
+from pandas import Series
+from scipy import stats
+from scipy.stats import norm 
+from numpy import arange,array,ones 
+import dash_table 
 import psycopg2
 
-
+current_year = datetime.now().year
 today = time.strftime("%Y-%m-%d")
 
 # df = pd.read_csv('https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&dataTypes=TMAX,TMIN&stations=USW00023062&startDate=2019-01-01&endDate=' + today + '&units=standard').round(1)
@@ -18,9 +26,9 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.config['suppress_callback_exceptions']=True
 
 # year list for dropdown selector
-# year = []
-# for YEAR in df.index.year.unique():
-#     year.append({'label':(YEAR), 'value':YEAR})
+year = []
+for YEAR in range(1950, current_year):
+    year.append({'label':(YEAR), 'value':YEAR})
 
 
 
@@ -52,29 +60,29 @@ def getRecordTemp(month, day):
 
 getRecordTemp(2, 2)
 
-def getMaxTempYear():
-    try:
-        connection = psycopg2.connect(user = "postgres",
-                                    password = "1234",
-                                    host = "localhost",
-                                    database = "denver_temps")
-        cursor = connection.cursor()
-        postgreSQL_select_Query = 'SELECT * FROM temps WHERE EXTRACT(year FROM "DATE"::TIMESTAMP) = 1972'
+# def getMaxTempYear():
+#     try:
+#         connection = psycopg2.connect(user = "postgres",
+#                                     password = "1234",
+#                                     host = "localhost",
+#                                     database = "denver_temps")
+#         cursor = connection.cursor()
+#         postgreSQL_select_Query = 'SELECT * FROM temps WHERE EXTRACT(year FROM "DATE"::TIMESTAMP) = 1972'
 
-        cursor.execute(postgreSQL_select_Query)
-        temp_records = cursor.fetchall()
-        print(temp_records)
+#         cursor.execute(postgreSQL_select_Query)
+#         temp_records = cursor.fetchall()
+#         print(temp_records)
 
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while fetching data from PostgreSQL", error)
+#     except (Exception, psycopg2.Error) as error :
+#         print ("Error while fetching data from PostgreSQL", error)
     
-    finally:
-        #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-getMaxTempYear()
+#     finally:
+#         #closing database connection.
+#         if(connection):
+#             cursor.close()
+#             connection.close()
+#             print("PostgreSQL connection is closed")
+# getMaxTempYear()
 
 
 body = dbc.Container([
@@ -93,21 +101,68 @@ body = dbc.Container([
             html.H5('SELECT YEAR', style={'text-align':'center'})
         ),
     ]),
-    # dbc.Row([
-    #     dbc.Col(
-    #         dcc.Dropdown(id='year-picker', options=year
-    #         ),
-    #         width = {'size': 3}), 
-    # ],
-    # justify='around',
-    # )
+    dbc.Row([
+        dbc.Col(
+            dcc.Dropdown(id='year-picker', options=year
+            ),
+            width = {'size': 3}), 
+    ],
+    justify='around',
+    )
 ])
 
 
 
-# @app.callback(Output('graph', 'figure'),
-#             [Input('year-picker', 'value')])
-# def update_figure(selected_year):
+@app.callback(Output('graph1', 'figure'),
+             [Input('year-picker', 'value')])
+def update_figure(selected_year):
+    traces = []
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password = "1234",
+                                    host = "localhost",
+                                    database = "denver_temps")
+        cursor = connection.cursor()
+        postgreSQL_select_Query = 'SELECT * FROM temps WHERE EXTRACT(year FROM "DATE"::TIMESTAMP) = {}'.format(selected_year)
+
+        cursor.execute(postgreSQL_select_Query)
+        temp_records = cursor.fetchall()
+        print(temp_records)
+        data = pd.DataFrame(temp_records)
+        print(data.head(10))
+
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while fetching data from PostgreSQL", error)
+    
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    # traces.append(go.Scatter(
+    #     y = year_param_min,
+    #     name = param,
+    #     line = {'color':'dodgerblue'}
+    #     ))
+
+    # return {
+    #     'data': traces,
+    #     'layout': go.Layout(
+    #         xaxis = {'title': 'DAY'},
+    #         yaxis = {'title': 'TEMP'},
+    #         hovermode = 'closest',
+    #         title = 'Daily Temps',
+    #         height = 400,
+
+    #     )
+    # }
+
+
+
+
 
 
 

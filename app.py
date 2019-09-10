aa2e47b49a0018cb420d6d20374f54beb232a7ef
James Swank <jmswank7@gmail.com>
2019-09-10 10:46:53 -0600

    starts dynamic layout

--- app.py
+++ app.py
@@ -30,6 +30,118 @@ year = []
 for YEAR in range(1950, current_year+1):
     year.append({'label':(YEAR), 'value':YEAR})
 
+body = dbc.Container([
+    dbc.Row([
+        dbc.Col(
+            html.Div(
+                className='app-header',
+                children=[
+                    html.Div('DENVER TEMPERATURE RECORD', className="app-header--title"),
+                ]
+            ),
+        ),
+    ]),
+    dbc.Row([
+        dbc.Col(
+            dcc.RadioItems(id='product', options=[
+                {'label':'Calendar day summaries', 'value':'Calendar day summaries'},
+                {'label':'Temperature graphs', 'value':'Temperature graphs'}
+            ]
+            ),
+            width = {'size': 3}
+    # justify='around',
+    ),
+        dbc.Col(
+            dcc.Input(
+                id='year',
+                value = 0,
+                type = "number"
+            ),
+            width = {'size': 3}
+        ),
+    ]),
+    dbc.Row([
+        dbc.Col(
+            html.Div([
+                dcc.Graph(id='graph1'),
+            ]),
+            width={'size':9}
+        ),
+    ],
+    # justify='around',
+    ),
+    dbc.Row([
+        dbc.Col(
+            html.H5('SELECT YEAR', style={'text-align':'center'})
+        ),
+    ]),
+    dbc.Row([
+        dbc.Col(
+            dcc.Dropdown(id='year-picker', options=year
+            ),
+            width = {'size': 3}), 
+    ],
+    justify='around',
+    )
+])
+
+
+
+
+@app.callback(Output('graph1', 'figure'),
+             [Input('year-picker', 'value')])
+def update_figure(selected_year):
+    traces = []
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password = "1234",
+                                    host = "localhost",
+                                    database = "denver_temps")
+        cursor = connection.cursor()
+        postgreSQL_select_Query = 'SELECT * FROM temps WHERE EXTRACT(year FROM "DATE"::TIMESTAMP) = {}'.format(selected_year)
+
+        cursor.execute(postgreSQL_select_Query)
+        temp_records = cursor.fetchall()
+        # print(temp_records)
+        df = pd.DataFrame(temp_records)
+        df[5] = df[3] - df[4]
+        print(data.head(10))
+        daily_temp_range = df[3]
+        # print(daily_max)
+        
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while fetching data from PostgreSQL", error)
+    
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    trace = [
+        go.Bar(
+            y = df[5],
+            base = df[4],
+            marker = {'color':'dodgerblue'},
+            hovertemplate = "<b>STUFF</b>"
+        )
+    ]
+    layout = go.Layout(
+            xaxis = {'rangeslider': {'visible':True},},
+            yaxis = {"title": 'Temperature F'},
+            title ='Daily Temps',
+            plot_bgcolor = 'lightgray',
+            height = 900,
+        )
+    return {'data': trace, 'layout': layout}
+   
+app.layout = html.Div(body)
+
+if __name__ == "__main__":
+    app.run_server(port=8050, debug=False)
+
+
 # def getRecordTemp(month, day):
 #     try:
 #         connection = psycopg2.connect(user = "postgres",
@@ -57,90 +169,3 @@ for YEAR in range(1950, current_year+1):
 #             print("PostgreSQL connection is closed")
 
 # getRecordTemp(2, 2)
-
-body = dbc.Container([
-    dbc.Row([
-        dbc.Col(
-            html.Div([
-                dcc.Graph(id='graph1'),
-            ]),
-            width={'size':12}
-        ),
-    ],
-    # justify='around',
-    ),
-    dbc.Row([
-        dbc.Col(
-            html.H5('SELECT YEAR', style={'text-align':'center'})
-        ),
-    ]),
-    dbc.Row([
-        dbc.Col(
-            dcc.Dropdown(id='year-picker', options=year
-            ),
-            width = {'size': 3}), 
-    ],
-    justify='around',
-    )
-])
-
-
-@app.callback(Output('graph1', 'figure'),
-             [Input('year-picker', 'value')])
-def update_figure(selected_year):
-    traces = []
-    try:
-        connection = psycopg2.connect(user = "postgres",
-                                    password = "1234",
-                                    host = "localhost",
-                                    database = "denver_temps")
-        cursor = connection.cursor()
-        postgreSQL_select_Query = 'SELECT * FROM temps WHERE EXTRACT(year FROM "DATE"::TIMESTAMP) = {}'.format(selected_year)
-
-        cursor.execute(postgreSQL_select_Query)
-        temp_records = cursor.fetchall()
-        # print(temp_records)
-        data = pd.DataFrame(temp_records)
-        data[5] = data[3] - data[4]
-        print(data.head(10))
-        daily_temp_range = data[3]
-        # print(daily_max)
-        top = data[3].tolist()
-        # print(top)
-        
-        bottom = data[4].tolist()
-        # print(bottom)
-        t = list(map(operator.sub, top, bottom))
-        # print(t)
-        ran = []
-        
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while fetching data from PostgreSQL", error)
-    
-    finally:
-        #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
-    trace = [
-        go.Bar(
-            y=data[5],
-            base=bottom,
-            marker = {'color':'dodgerblue'}
-        )
-    ]
-    layout = go.Layout(
-            xaxis={'rangeslider': {'visible':True},},
-            yaxis={"title": 'stuff'},
-            title='Daily Temps',
-            plot_bgcolor = 'lightgray',
-            height = 900,
-        )
-    return {'data': trace, 'layout': layout}
-   
-app.layout = html.Div(body)
-
-if __name__ == "__main__":
-    app.run_server(port=8050, debug=False)

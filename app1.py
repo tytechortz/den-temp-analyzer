7460ab18c876594e62e67bb234668d0d3ec3eef1
James Swank <jmswank7@gmail.com>
2019-09-18 19:48:23 -0600

    displays all temps

--- app1.py
+++ app1.py
@@ -16,6 +16,7 @@ import dash_table
 import psycopg2
 import operator
 from dash.exceptions import PreventUpdate
+import json
 
 current_year = datetime.now().year
 today = time.strftime("%Y-%m-%d")
@@ -94,7 +95,84 @@ body = dbc.Container([
             html.H5('SELECT YEAR', style={'text-align':'center'})
         ),
     ]),
+    html.Div(id='temp-data', style={'display': 'none'})
 ])
+
+@app.callback(Output('temp-data', 'children'),
+             [Input('year', 'value'),
+             Input('period', 'value')])
+def update_data(selected_year, period):
+    
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password = "1234",
+                                    host = "localhost",
+                                    database = "denver_temps")
+
+        cursor = connection.cursor()
+
+        postgreSQL_select_year_Query = 'SELECT * FROM temps WHERE EXTRACT(year FROM "DATE"::TIMESTAMP) = {}'.format(selected_year)
+        cursor.execute(postgreSQL_select_year_Query)
+        temp_records = cursor.fetchall()
+        df = pd.DataFrame(temp_records)
+        
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while fetching data from PostgreSQL", error)
+    
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    return df.to_json()
+
+@app.callback(Output('graph1', 'figure'),
+             [Input('temp-data', 'children'),
+             Input('period', 'value')])
+def update_figure(temp_data, period):
+    temps = pd.read_json(temp_data)
+    temps[5] = temps[3] - temps[4]
+    print(temps)
+    trace = [
+            go.Bar(
+                y = temps[5],
+                # x = data_period,
+                base = temps[4],
+                marker = {'color':'dodgerblue'},
+                hovertemplate = "<b>STUFF</b>"
+            ),
+            # go.Scatter(
+            #     y = high_norms,
+            # ),
+            # go.Scatter(
+            #     y = low_norms
+            # ),
+            # go.Scatter(
+            #     y = df_record_highs_ly[0]
+            # ),
+            # go.Scatter(
+            #     y = df_record_lows_ly[0]
+            # ),
+        ]
+    layout = go.Layout(
+                xaxis = {'rangeslider': {'visible':True},},
+                yaxis = {"title": 'Temperature F'},
+                title ='Daily Temps',
+                plot_bgcolor = 'lightgray',
+                height = 700,
+        )
+    return {'data': trace, 'layout': layout}
+
+@app.callback(
+    Output('time-param', 'children'),
+    [Input('product', 'value')])
+def display_time_param(product_value):
+    if product_value == 'daily-data-month':
+        return html.Div('Date:')
+    elif product_value == 'temp-graph':
+        return html.Div([html.H3('Year:')])
 
 @app.callback(
     Output('year-picker', 'children'),
@@ -109,6 +187,45 @@ def display_year_selector(product_value):
                 )
     elif product_value == 'daily-data-month':
         return
+
+@app.callback(
+    Output('graph-stuff', 'children'),
+    [Input('product', 'value')])
+def display_graph(value):
+    if value == 'temp-graph':
+        return dcc.Graph(id='graph1')
+
+@app.callback(
+    Output('graph-info-row', 'children'),
+    [Input('product', 'value')])
+def display_graph_info_row(product_value):
+    print(product_value)
+    if product_value == 'temp-graph':
+        return html.Div('Select Period')
+
+@app.callback(
+    Output('period-picker', 'children'),
+    [Input('product', 'value')])
+    # Input('year', 'value')])
+   
+def display_period_selector(product_value):
+    if product_value == 'temp-graph':
+        # def labeler():
+        #     return 
+        return  dcc.RadioItems(
+                    id = 'period',
+                    options = [
+                        {'label':'Annual (Jan-Dec)', 'value':'annual'},
+                        {'label':'Spring (Mar-May)', 'value':'spring'},
+                        {'label':'Summer (Jun-Aug)', 'value':'summer'},
+                        {'label':'Fall (Sep-Nov)', 'value':'fall'},
+                        {'label':'Winter (Dec-Feb)', 'value':'winter'},
+                    ],
+                    value = 'annual',
+                    labelStyle = {'display':'block'}
+                )
+
+
 
 app.layout = html.Div(body)
 

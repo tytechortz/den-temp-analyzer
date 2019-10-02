98f9087726b8e53fcc8c3fe9eb75a4eb7c00ac88
James Swank <jmswank7@gmail.com>
2019-10-01 20:04:39 -0600

    all working

--- app1.py
+++ app1.py
@@ -209,7 +209,11 @@ def all_temps(selected_year, period):
 @app.callback(Output('rec-highs', 'children'),
              [Input('year', 'value')])
 def rec_high_temps(selected_year):
-    return df_rec_highs.to_json()
+    if int(selected_year) % 4 == 0:
+        rec_highs = df_rec_highs
+    else:
+        rec_highs = df_rec_highs.drop(df_rec_highs.index[59])
+    return rec_highs.to_json()
 
 @app.callback(Output('rec-lows', 'children'),
              [Input('year', 'value')])
@@ -290,20 +294,6 @@ def update_figure(temp_data, rec_highs, rec_lows, norms, selected_year, period):
     selected_year = selected_year
     temps = df_all_temps
 
-    date_range = []
-    date_time = []
-    sdate = date(int(previous_year), 12, 1)
-    edate = date(int(selected_year), 12, 31)
-
-    delta = edate - sdate
-
-    for i in range(delta.days + 1):
-        day = sdate + timedelta(days=i)
-        date_range.append(day)
-    for j in date_range:
-        day = j.strftime("%Y-%m-%d")
-        date_time.append(day)
-
     temps[6] = temps.index.day_name()
     temps[5] = temps[3] - temps[4]
    
@@ -315,76 +305,99 @@ def update_figure(temp_data, rec_highs, rec_lows, norms, selected_year, period):
     df_record_lows_ly = df_record_lows_ly.set_index(1)
     df_rl_cy = df_record_lows_ly[:len(temps_cy.index)]
     df_rh_cy = df_record_highs_ly[:len(temps_cy.index)]
-    print(df_rl_cy)
+    # print(df_rl_cy)
     df_norms = pd.read_json(norms)
     df_norms_cy = df_norms[:len(temps_cy.index)]
-    print(df_norms)
+    # print(df_norms)
     # print(df_record_lows_cy)
     temps_cy.loc[:,'rl'] = df_rl_cy[0].values
     temps_cy.loc[:,'rh'] = df_rh_cy[0].values
     temps_cy.loc[:,'nh'] = df_norms_cy[3].values
     temps_cy.loc[:,'nl'] = df_norms_cy[4].values
-    print(temps_cy)
+    # print(temps_cy)
+
+
     if period == 'spring':
         temps = temps_cy[temps_cy.index.month.isin([3,4,5])]
-        df_record_highs_ly = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(03-)|(04-)|(05-)')]
-        df_record_lows_ly = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(03-)|(04-)|(05-)')]
-        df_high_norms = df_norms[3][90:182]
-        df_low_norms = df_norms[4][90:182]
-        date_time = date_time[90:182]
-        bar_x = date_time
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
       
     elif period == 'summer':
         temps = temps_cy[temps_cy.index.month.isin([6,7,8])]
-        df_record_highs_ly = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(06-)|(07-)|(08-)')]
-        df_record_lows_ly = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(06-)|(07-)|(08-)')]
-        df_high_norms = df_norms[3][151:244]
-        df_low_norms = df_norms[4][151:244]
-        date_time = date_time[183:275]
-        bar_x = date_time
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
 
     elif period == 'fall':
         temps = temps_cy[temps_cy.index.month.isin([9,10,11])]
-        df_record_highs_ly = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(09-)|(10-)|(11-)')]
-        df_record_lows_ly = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(09-)|(10-)|(11-)')]
-        df_high_norms = df_norms[3][243:335]
-        df_low_norms = df_norms[4][243:335]
-        date_time = date_time[275:366]
-        bar_x = date_time
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
 
     elif period == 'winter':
+        date_range = []
+        date_time = []
+        sdate = date(int(previous_year), 12, 1)
+        edate = date(int(selected_year), 12, 31)
+
+        delta = edate - sdate
+
+        for i in range(delta.days + 1):
+            day = sdate + timedelta(days=i)
+            date_range.append(day)
+        for j in date_range:
+            day = j.strftime("%Y-%m-%d")
+            date_time.append(day)
+
         temps_py = temps_py[temps_py.index.month.isin([12])]
         temps_cy = temps_cy[temps_cy.index.month.isin([1,2])]
         temp_frames = [temps_py, temps_cy]
         temps = pd.concat(temp_frames)
         date_time = date_time[:91]  
-
+        # print(temps)
+        
         df_record_highs_jan_feb = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(01-)|(02-)')]
         df_record_highs_dec = df_record_highs_ly[df_record_highs_ly.index.str.match(pat = '(12-)')]
         high_frames = [df_record_highs_dec, df_record_highs_jan_feb]
-        df_record_highs_ly = pd.concat(high_frames)
+        df_record_highs = pd.concat(high_frames)
+
         df_record_lows_jan_feb = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(01-)|(02-)')]
         df_record_lows_dec = df_record_lows_ly[df_record_lows_ly.index.str.match(pat = '(12-)')]
         low_frames = [df_record_lows_dec, df_record_lows_jan_feb]
-        df_record_lows_ly = pd.concat(low_frames)
+        df_record_lows = pd.concat(low_frames)
+
         df_high_norms_jan_feb = df_norms[3][0:60]
         df_high_norms_dec = df_norms[3][335:]
         high_norm_frames = [df_high_norms_dec, df_high_norms_jan_feb]
         df_high_norms = pd.concat(high_norm_frames)
+
         df_low_norms_jan_feb = df_norms[4][0:60]
         df_low_norms_dec = df_norms[4][335:]
         low_norm_frames = [df_low_norms_dec, df_low_norms_jan_feb]
         df_low_norms = pd.concat(low_norm_frames)
+
         bar_x = date_time
+        nh_value = df_high_norms
+        nl_value = df_low_norms
+        rh_value = df_record_highs[0]
+        rl_value = df_record_lows[0]
 
     else:
         temps = temps_cy
-        df_high_norms = df_norms[3]
-        df_low_norms = df_norms[4]
-        date_time = date_time[31:]
-        bar_x = temps_cy.index
+        nh_value = temps['nh']
+        nl_value = temps['nl']
+        rh_value = temps['rh']
+        rl_value = temps['rl']
+        bar_x = temps.index
 
-    mkr_color = {'color':'paleturquoise'}
+    mkr_color = {'color':'black'}
       
     trace = [
             go.Bar(
@@ -396,31 +409,37 @@ def update_figure(temp_data, rec_highs, rec_lows, norms, selected_year, period):
                 hovertemplate = 'Temp Range: %{y} - %{base}<extra></extra>'                  
             ),
             go.Scatter(
-                y = df_high_norms,
-                x = date_time,
+                # y = df_high_norms,
+                y = nh_value,
+                # x = date_time,
+                x = bar_x,
                 # hoverinfo='none',
                 name='Normal High',
                 marker = {'color':'indianred'}
             ),
             go.Scatter(
-                y = df_low_norms,
-                x = date_time,
+                # y = df_low_norms,
+                y = nl_value,
+                # x = date_time,
+                x = bar_x,
                 # hoverinfo='none',
                 name='Normal Low',
                 marker = {'color':'slateblue'}
             ),
             go.Scatter(
                 # y = df_record_highs_ly[0],
-                y = temps['rh'],
-                x = date_time,
+                y = rh_value,
+                # x = date_time,
+                x = bar_x,
                 # hoverinfo='none',
                 name='Record High',
                 marker = {'color':'red'}
             ),
             go.Scatter(
                 # y = df_record_lows_ly[0],
-                y = temps['rl'],
-                x = date_time,
+                y = rl_value,
+                # x = date_time,
+                x = bar_x,
                 # hoverinfo='none',
                 name='Record Low',
                 marker = {'color':'blue'}

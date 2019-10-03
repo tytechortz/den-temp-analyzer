be62cdf0787d50fd3f65b55e958da1d8b0ab28dd
James Swank <jmswank7@gmail.com>
2019-10-02 20:05:30 -0600

    adds period picker

--- app2.py
+++ app2.py
@@ -2,10 +2,15 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+from datetime import datetime, date, timedelta
+import json, csv, psycopg2, dash_table, time, operator
 
 
 app = dash.Dash(__name__)
 app.config['suppress_callback_exceptions']=True
+
+current_year = datetime.now().year
+today = time.strftime("%Y-%m-%d")
 
 
 app.layout = html.Div(
@@ -39,6 +44,9 @@ app.layout = html.Div(
             html.Div([
                 html.Label('Options'),
                 html.Div(
+                    id='period-picker'
+                ),
+                html.Div(
                     id='year-picker'
                 ),
             ],
@@ -69,6 +77,49 @@ def display_time_param(product_value):
         return html.Div('Date:')
     elif product_value == 'temp-graph':
         return html.Div([html.H3('Year:')])
+
+@app.callback(
+    Output('year-picker', 'children'),
+    [Input('product', 'value')])
+def display_year_selector(product_value):
+    if product_value == 'temp-graph' or 'fyma':
+        return dcc.Input(
+                    id = 'year',
+                    type = 'number',
+                    value = str(current_year),
+                    min = 1950, max = current_year
+                )
+    elif product_value == 'daily-data-month':
+        return
+
+@app.callback(
+    Output('period-picker', 'children'),
+    [Input('product', 'value')])
+    # Input('year', 'value')])
+def display_period_selector(product_value):
+    if product_value == 'temp-graph':
+        return  dcc.RadioItems(
+                    id = 'period',
+                    options = [
+                        {'label':'Annual (Jan-Dec)', 'value':'annual'},
+                        {'label':'Spring (Mar-May)', 'value':'spring'},
+                        {'label':'Summer (Jun-Aug)', 'value':'summer'},
+                        {'label':'Fall (Sep-Nov)', 'value':'fall'},
+                        {'label':'Winter (Dec-Feb)', 'value':'winter'},
+                    ],
+                    value = 'annual',
+                    labelStyle = {'display':'block'}
+                )
+    elif product_value == 'fyma':
+        return  dcc.RadioItems(
+                    id = 'temp-param',
+                    options = [
+                        {'label':'Max Temp', 'value':'Tmax'},
+                        {'label':'Min Temp', 'value':'Tmin'},
+                    ],
+                    value = 'Tmax',
+                    labelStyle = {'display':'block'}
+                )
 
 
 if __name__ == "__main__":

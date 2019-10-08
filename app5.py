7230d1799e8050ced4b7a44506c0a11da378fd5e
James Swank <jmswank7@gmail.com>
2019-10-08 09:29:48 -0600

    adds product selection and title date works

--- app5.py
+++ app5.py
@@ -36,12 +36,101 @@ app.layout = html.Div(
         ],
             className='row'
         ),
+        html.Div([
+            html.H4(
+                id='title-date-range',
+                className='twelve columns',
+                style={'text-align': 'center'}
+            ),
+        ],
+            className='row'
+        ),
+        html.Hr(style={'margin': '0', 'margin-bottom': '5'}),
+        html.Div([
+            html.Div([
+                html.Label('Select Product'),
+                dcc.RadioItems(
+                    id='product',
+                    options=[
+                        {'label':'Temperature graphs', 'value':'temp-graph'},
+                        {'label':'Climatology for a day', 'value':'climate-for-day'},
+                        {'label':'5 Year Moving Avgs', 'value':'fyma'},
+                    ],
+                    value='temp-graph',
+                    labelStyle={'display': 'block'},
+                ),
+            ],
+                className='three columns',
+            ),
+            html.Div([
+                html.Label('Options'),
+                html.Div(
+                    id='period-picker'
+                ),
+                html.Div(
+                    id='year-picker'
+                ),
+                html.Div(
+                    id='date-picker'
+                ),
+            ],
+                className='two-columns',
+            ),  
+        ],
+            className='row'
+        ),
+        html.Div(id='all-data', style={'display': 'none'}),
     ]
 )
        
-     
+@app.callback(Output('title-date-range', 'children'),
+            [Input('all-data', 'children')])
+def all_temps_cleaner(temps):
+    title_temps = pd.read_json(temps)
+    title_temps['Date']=title_temps['Date'].dt.strftime("%Y-%m-%d")
+    last_day = title_temps.iloc[-1, 0] 
+    
+    return '1950-01-01 through {}'.format(last_day)   
+@app.callback(Output('all-data', 'children'),
+            [Input('product', 'value')])
+def all_temps_cleaner(product):
+    # print(product)
+    cleaned_all_temps = df_all_temps
+    cleaned_all_temps.columns=['dow','sta','Date','TMAX','TMIN']
+    cleaned_all_temps['Date'] = pd.to_datetime(cleaned_all_temps['Date'])
+    cleaned_all_temps = cleaned_all_temps.drop(['dow','sta'], axis=1)
 
+    return cleaned_all_temps.to_json(date_format='iso')
+
+@app.callback(Output('temp-data', 'children'),
+             [Input('year', 'value'),
+             Input('period', 'value')])
+def all_temps(selected_year, period):
+    previous_year = int(selected_year) - 1
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password = "1234",
+                                    host = "localhost",
+                                    database = "denver_temps")
+        cursor = connection.cursor()
+
+        postgreSQL_select_year_Query = 'SELECT * FROM temps WHERE EXTRACT(year FROM "DATE"::TIMESTAMP) IN ({},{}) ORDER BY "DATE" ASC'.format(selected_year, previous_year)
+        cursor.execute(postgreSQL_select_year_Query)
+        temp_records = cursor.fetchall()
+        df = pd.DataFrame(temp_records)
+        
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while fetching data from PostgreSQL", error)
+    
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    return df.to_json()
 
 if __name__ == "__main__":
-    app.run_server(port=8050, debug=False)
+    app.run_server(port=8050, debug=True)
 
